50f7444471f6f8d3ab8e012a077414f6cf09ea04
Kevin Bon <bon.kevin@gmail.com>
2023-12-23 14:13:04 -0500

    hum

--- src/app.js
+++ src/app.js
@@ -17,7 +17,7 @@ export async function start(options) {
 
   parser.init();
   parser.start();
-  // parser.on("data", bgObserver.onData);
+  parser.on("data", getObserver().onData);
   parser.on("update", getObserver().onData);
 
   return () => {
